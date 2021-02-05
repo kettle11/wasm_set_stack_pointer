dc362e495ad54e422e5983902bdd10c508327cfa
Ian Kettlewell <ian.kettlewell@gmail.com>
2021-02-05 17:30:46 -0500

    Remove commented out code

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,3 @@ extern "C" {
     #[doc(hidden)]
     pub fn set_stack_pointer(i: i32);
 }
-
-// This is just to ensure that function is included in the output binary.
-// There's probably a better way to ensure that.
-/*
-#[doc(hidden)]
-#[no_mangle]
-pub extern "C" fn __do_not_call() {
-    unsafe {
-        set_stack_pointer(0);
-    }
-}
-*/
